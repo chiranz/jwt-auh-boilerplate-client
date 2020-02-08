8e8194105260c3db93cc470f6602aeba84a2d8dd
chiranz <chiranpoudyal@gmail.com>
2020-02-08 10:44:13 +0530

    Client setup wit apollo boost

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
 
 const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const { data, loading } = useQuery(gql`
+    {
+      hello
+    }
+  `);
+  if (loading) return <h1>Loading...</h1>;
+  return <h1>{JSON.stringify(data)}</h1>;
+};
 
 export default App;
